346b7acd2179a24c9caca3624a16b81612721980
Gopal Bonthu <62247609+gbonthu@users.noreply.github.com>
2022-06-28 08:48:31 -0400

    CSM-4165: Configure child resources with new uptycs' provider (#2)
    * CSM-4165: Create child resources with new uptycs' provider
    
    * Move profile attribute from provider to resource
    
    * CSM-4165: remove hardcoded account list

--- providers.tf
+++ providers.tf
@@ -1,10 +1,13 @@
 terraform {
   required_version = ">= 0.12.21"
-
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = ">= 3.50.0"
+    }
+    uptycscspm = {
+      source  = "github.com/uptycslabs/uptycscspm"
+      version = "0.0.1"
     }
   }
 }
