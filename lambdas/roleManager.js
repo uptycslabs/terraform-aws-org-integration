46f94921a4ca18bc627eadb3a635844e176c746c
Matthew Ellis <100235733+mellis-uptycs@users.noreply.github.com>
2022-12-14 20:50:07 +0000

    CSM-6197: Auto-integration Fix (#10)
    * Auto-integration goes through every record in event
    
    * Switched to forEach loop
    
    * Update to error checking
    
    * CSM-6197: repalce foreach with for
    
    Co-authored-by: Gopal B <gbonthu@uptycs.com>

--- lambdas/roleManager.js
+++ lambdas/roleManager.js
@@ -5,70 +5,83 @@ const utils = require('./utils');
 module.exports.handler = async (event, context, callback) => {
     console.log('Received event: ', JSON.stringify(event, null, 2));
 
-    if (!event || !event.Records[0] || !event.Records[0].body) {
+    if (!event) {
         callback(new Error('No event specified'));
         return;
     }
 
-    const req = JSON.parse(event.Records[0].body);
-    // Validate RequestType
-    if (!req.RequestType || (req.RequestType !== 'Create' && req.RequestType !== 'Delete')) {
-        callback(new Error('Invalid RequestType. Valid values: "Create" or "Delete"'));
-        return;
-    }
-
-    // Check if all required attributes are set
-    if (!req.IntegrationName || !req.AccountIds || !req.UptAccountId || !req.ExternalId || !req.RequestType) {
-        callback(new Error(`Request:${JSON.stringify(req, null, 2)} is not valid. Required fields are missing`));
-        return;
-    }
-
-    // Currently we only support one account at a time
-    if (req.AccountIds.length !== 1) {
-        callback(new Error('AccountIds must be an array of strings with length of 1'));
-        return;
-    }
-
-    const stsCreds = await utils.stsCreds(req.AccountIds[0]);
-    const iamClient = await utils.iamClient(stsCreds, 'aws-global');
-    const sqsClient = await utils.sqsClient(event.Records[0].awsRegion);
-
-    const response = {
-        message: 'OK',
-        accountId: req.AccountIds[0],
-        integrationName: req.IntegrationName,
-        requestMessageId: event.Records[0].messageId,
-        timestamp: Date.now
-      };
-
     try {
-        if (req.RequestType === 'Delete') {
-            await utils.detachPoliciesFromRole(iamClient, req.IntegrationName);
-            await utils.deleteIntegrationRole(iamClient, req.IntegrationName);
-            console.log(`Successfully deleted integration role ${req.IntegrationName} from account ${req.AccountIds[0]}`);
-            await utils.sendResponse(sqsClient, event.Records[0].eventSourceARN, req.IntegrationName, response);
-            return;
+        for (let i = 0; i < event.Records.length; i++) {
+            const record = event.Records[i];
+            if (!record || !record.body) {
+                throw(new Error('No event specified'));
+            }
+            const req = JSON.parse(record.body);
+            // Validate RequestType
+            if (!req.RequestType || (req.RequestType !== 'Create' && req.RequestType !== 'Delete')) {
+                throw(new Error('Invalid RequestType. Valid values: "Create" or "Delete"'));
+            }
+            // Check if all required attributes are set
+            if (!req.IntegrationName || !req.AccountIds || !req.UptAccountId || !req.ExternalId || !req.RequestType) {
+                throw(new Error(`Request:${JSON.stringify(req, null, 2)} is not valid. Required fields are missing`));
+            }
+
+            // Currently we only support one account at a time
+            if (req.AccountIds.length !== 1) {
+                throw(new Error('AccountIds must be an array of strings with length of 1'));
+            }
         }
-        // Continue with "Create"
-        const roleExists = await utils.integrationRoleExists(iamClient, req.IntegrationName);
-        // Check if we have to replace existing role
-        if (req.Force && roleExists) {
-            await utils.detachPoliciesFromRole(iamClient, req.IntegrationName);
-            await utils.deleteIntegrationRole(iamClient, req.IntegrationName);
-        } else if (roleExists) {
-            const msg = `Role ${req.IntegrationName} already exists`;
-            response.message = msg;
-            console.error(msg);
-            await utils.sendResponse(sqsClient, event.Records[0].eventSourceARN, req.IntegrationName, response);
-            return;
+    } catch(err) {
+        callback(err);
+        return;
+    }
+
+    for (let i = 0; i < event.Records.length; i++) {
+        const record = event.Records[i];
+        const reqBody = JSON.parse(record.body);
+
+        const stsCreds = await utils.stsCreds(reqBody.AccountIds[0]);
+        const iamClient = await utils.iamClient(stsCreds, 'aws-global');
+        const sqsClient = await utils.sqsClient(record.awsRegion);
+
+        const response = {
+            message: 'OK',
+            accountId: reqBody.AccountIds[0],
+            integrationName: reqBody.IntegrationName,
+            requestMessageId: record.messageId,
+            timestamp: Date.now
+        };
+
+        try {
+            if (reqBody.RequestType === 'Delete') {
+                await utils.detachPoliciesFromRole(iamClient, reqBody.IntegrationName);
+                await utils.deleteIntegrationRole(iamClient, reqBody.IntegrationName);
+                console.log(`Successfully deleted integration role ${reqBody.IntegrationName} from account ${reqBody.AccountIds[0]}`);
+                await utils.sendResponse(sqsClient, record.eventSourceARN, reqBody.IntegrationName, response);
+                continue;
+            }
+            // Continue with "Create"
+            const roleExists = await utils.integrationRoleExists(iamClient, reqBody.IntegrationName);
+
+            // Check if we have to replace existing role
+            if (reqBody.Force && roleExists) {
+                await utils.detachPoliciesFromRole(iamClient, reqBody.IntegrationName);
+                await utils.deleteIntegrationRole(iamClient, reqBody.IntegrationName);
+            } else if (roleExists) {
+                const msg = `Role ${reqBody.IntegrationName} already exists`;
+                response.message = msg;
+                console.error(msg);
+                await utils.sendResponse(sqsClient, record.eventSourceARN, reqBody.IntegrationName, response);
+                continue;
+            }
+            await utils.createIntegrationRole(iamClient, reqBody.IntegrationName, reqBody.UptAccountId, reqBody.ExternalId);
+            await utils.attachPoliciesToRole(iamClient, reqBody.IntegrationName);
+            console.log(`Successfully installed integration role for ${reqBody.IntegrationName} in account ${reqBody.AccountIds[0]}`);
+            await utils.sendResponse(sqsClient, record.eventSourceARN, reqBody.IntegrationName, response);
+        } catch (err) {
+            response.message = 'Failed to invoke lambda function';
+            console.error('Failed to invoke lambda function', err);
+            await utils.sendResponse(sqsClient, record.eventSourceARN, reqBody.IntegrationName, response);
         }
-        await utils.createIntegrationRole(iamClient, req.IntegrationName, req.UptAccountId, req.ExternalId);
-        await utils.attachPoliciesToRole(iamClient, req.IntegrationName);
-        console.log(`Successfully installed integration role for ${req.IntegrationName} in account ${req.AccountIds[0]}`);
-        await utils.sendResponse(sqsClient,event.Records[0].eventSourceARN, req.IntegrationName, response);
-    } catch (err) {
-        response.message = 'Failed to invoke lambda function';
-        console.error('Failed to invoke lambda function', err);
-        await utils.sendResponse(sqsClient,event.Records[0].eventSourceARN, req.IntegrationName, response);
     }
 };
